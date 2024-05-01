ba71d1f0626338acf0bb4a6c8c246ab3a25c83fc
ru51a4 <rruslanvaleev@yandex.ru>
2024-05-01 14:52:04 +0500

    init

--- index.js
+++ index.js
@@ -4,89 +4,159 @@ class Query {
     joins = [];
     whereClauses = [];
     groupByColumns = [];
-    sortColumns = [];
-    limit;
-}
+    //sortColumns = [];
+    //limit;
+    static build = (input) => {
+        input = input.split("\n").join(" ").split(" ").map((s) => s.toUpperCase().split(",").join(""));
 
-let input = `SELECT * FROM diaries a JOIN ( SELECT diary_id, MAX(id) FROM posts p group by diary_id ) gg ON a.id = gg.di`;
-
-input = input.split(" ").map((s) => s.toUpperCase());
-let lex = (str) => {
-    let query = new Query();
-    let isColumns = false;
-    let isFromSources = false;
-    let join = false;
-    let group = false;
-    while (str.length) {
-        let token = str.shift();
-        if (token == "(") {
-            let _ = [];
-            let _t = str.shift();
-            while (_t !== ")") {
-                _.push(_t);
-                _t = str.shift();
-            }
-            token = lex(_);
-        }
-
-
-
-        if (token == "SELECT") {
-            isColumns = true;
-            continue;
-        }
-        if (isColumns) {
-            if (token == "FROM") {
-                isFromSources = true;
-                isColumns = false;
-                continue;
-            }
-            query.columns.push(token);
-            continue
-        }
-        if (isFromSources) {
-            if (token == "JOIN") {
-                join = true;
-                isFromSources = false;
-                continue;
-            }
-            if (str[0] !== "JOIN") {
-                let _ = str.shift();
-                query.fromSources.push({ "table": token, "name": _ });
-                if (str[0] !== "JOIN") {
-                    isFromSources = false;
+        let lex = (str) => {
+            let query = new Query();
+            let isColumns = false;
+            let isFromSources = false;
+            let isJoin = false;
+            let isWhere = false;
+            let isGroup = false;
+            while (str.length) {
+                let token = str.shift();
+                if (token === 'SELECT') {
+                    isColumns = true;
+                    continue
                 }
-                continue
-            }
-        }
-        if (join) {
-            console.log({ token })
-            if (token == "ON") {
-                let left = str.shift();
-                let exp = str.shift();
-                let right = str.shift();
-                query.joins[query.joins.length - 1].on = [left, exp, right];
-                join = false;
-                continue;
-            }
-            let _ = str.shift();
-            query.joins.push({ "table": token, "name": _ })
-            continue
-        }
-        if (token == "GROUP") {
-            query.groupByColumns = [];
-            group = true;
-            str.shift();
-            continue
-        }
-        if (group) {
-            query.groupByColumns.push(token);
-            group = false;
-            continue;
-        }
+                if (token === 'FROM') {
+                    isColumns = false;
+                    isFromSources = true;
+                    continue
+                }
+                if (token === 'JOIN') {
+                    isFromSources = false;
+                    isJoin = true;
+                    continue
+                }
+                if (token === 'WHERE') {
+                    isFromSources = false;
+                    isJoin = false;
+                    isWhere = true;
+                    continue
+                }
+                if (token === 'GROUP') {
+                    isFromSources = false;
+                    isJoin = false;
+                    isWhere = false;
+                    isGroup = true;
+                    str.shift();
+                    continue
+                }
+                if (isColumns) {
+                    query.columns.push(token)
+                    continue;
+                }
+                if (isFromSources) {
+                    query.fromSources.push(token)
+                    continue;
+                }
+                if (isJoin) {
+                    query.joins.push(token)
+                    continue;
+                }
 
+                if (isWhere) {
+                    query.whereClauses.push(token)
+                    continue;
+                }
+                if (isGroup) {
+                    query.groupByColumns.push(token)
+                }
+
+            }
+            let t = [];
+            for (let i = 0; i <= query.fromSources.length - 1; i = i + 2) {
+                t.push({ "table": query.fromSources[i], 'alias': query.fromSources[i + 1] })
+            }
+            query.fromSources = t;
+            t = [];
+            for (let i = 0; i <= query.joins.length - 1; i = i + 2) {
+                t.push({ "table": query.joins[i], 'alias': query.joins[i + 1] })
+                if (query.joins[i + 2] === 'ON' || query.joins[i + 2] === 'AND' || query.joins[i + 2] === 'OR') {
+                    t[t.length - 1].exp = [];
+                    t[t.length - 1].exp.push({ 'left': query.joins[i + 3], 'right': query.joins[i + 5], 'type': query.joins[i + 4], 'ttype': query.joins[i + 2] })
+                    i++;
+                    i++;
+                    i++;
+                    i++;
+                }
+            }
+            query.joins = t;
+
+            t = [];
+            for (let i = 0; i <= query.whereClauses.length - 1; i = i + 3) {
+                let next = (query.whereClauses[i + 3]);
+                if (next) {
+                    t.push({ "next": next, "left": query.whereClauses[i], 'right': query.whereClauses[i + 2], 'type': query.whereClauses[i + 1] })
+                    i++
+                }
+                else {
+                    t.push({ "left": query.whereClauses[i], 'right': query.whereClauses[i + 2], 'type': query.whereClauses[i + 1] })
+                }
+            }
+            query.whereClauses = t;
+            t = [];
+            for (let i = 0; i <= query.groupByColumns.length - 1; i = i + 2) {
+                t.push({ "col": query.groupByColumns[i], 'type': query.groupByColumns[i + 1] })
+            }
+            query.groupByColumns = t;
+            return query;
+        };
+
+
+        let t = [[]];
+        let nested = (str) => {
+            let tt = [];
+            while (str.length) {
+                let token = str.shift();
+                if (token.includes('(')) {
+                    t[t.length - 1].push(...tt);
+                    tt = [];
+                    t.push([])
+                }
+                else if (token.includes(')')) {
+                    t[t.length - 1].push(...tt);
+                    tt = [];
+                    //
+                    let c = t[t.length - 1];
+                    t.pop();
+                    t[t.length - 1].push(c);
+                } else {
+                    tt.push(token)
+                }
+            }
+
+        };
+        nested(input);
+
+        let calc = (c) => {
+            c.splice(0, c.length - 1, lex(c))
+        }
+        let deep = (arr, init = true) => {
+            for (let i = 0; i <= arr.length - 1; i++) {
+                if (Array.isArray(arr[i])) {
+                    if (arr[i].length === 1) {
+                        arr[i] = arr[i][0];
+                    } else {
+                        deep(arr[i], 0);
+                        i = i - 1;
+                    }
+                }
+            }
+            if (!init) {
+                calc(arr)
+            }
+        }
+        deep(t[0]);
+        t = t[0]
+        return t
     }
-    return query;
-};
-
-console.log(lex(input));
+}
+function getObject(input) {
+    let q = new Query();
+    return (Query.build(input))
+}
